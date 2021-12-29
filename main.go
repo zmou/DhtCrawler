5bf3b97703ec03059baf26e764a53ab241841a33
郑蓬勃 <571821527@qq.com>
2021-12-29 20:38:23 +0800

    hashId写入文件

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"DhtCrawler/dht"
 	"fmt"
+	"io"
 	"os"
+	"time"
 )
 
 func main() {
@@ -25,13 +27,32 @@ func main() {
 
 	for {
 		select {
-
 		//输出爬虫抓取的HashId结果
 		case hashId := <-outHashIdChan:
 			fmt.Println(hashId)
+
+			// 写入文件
+			writeToFile(hashId)
 
 		case msg := <-master:
 			fmt.Println(msg)
 		}
 	}
+}
+
+func writeToFile(hashId string) {
+	logPath := "info_hash/" + time.Now().Format("2006-01-02") + ".log"
+	f, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+	if err != nil {
+		return
+	}
+
+	hashId = fmt.Sprintf("%s\r\n", hashId)
+
+	_, err = io.WriteString(f, hashId)
+
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
 }
